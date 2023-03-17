316457012470a1ab67ba34942b4703b9b429b708
Aumi0 <128075224+Aumi0@users.noreply.github.com>
2023-03-17 17:10:54 +0000

    done

---
+++ bruh.java
@@ -0,0 +1,11 @@
+  /**
+   * bruh
+   */
+  public class bruh {
+  
+    public static void main(String[] args) {
+        for(int i=0; i<40; i++){
+            System.out.println("testing");
+        }
+    }
+  }
