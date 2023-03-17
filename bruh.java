0d1778313c51e0061e2a9e16a654eb66d90be4bc
aumio <aminb3691@gmail.com>
2023-03-17 23:36:46 +0600

    with while

--- bruh.java
+++ bruh.java
@@ -4,13 +4,23 @@ public class bruh {
   
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter the number of times to print 'testing': ");
-    int numTimes = scanner.nextInt();
     
-    int i = 0;
-    while(i < numTimes){
-      System.out.println("testing");
-      i++;
+    while (true) {
+      System.out.print("Enter the number of times to print 'testing', or enter 'q' to quit: ");
+      
+      String userInput = scanner.nextLine();
+      
+      if(userInput.equals("q")) {
+        break;
+      }
+      
+      int numTimes = Integer.parseInt(userInput);
+        
+      int i = 0;
+      while(i < numTimes){
+        System.out.println("testing");
+        i++;
+      }
     }
     
     scanner.close();
