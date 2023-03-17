b206be28ab44fd390ba6251218f92f43bb431e13
Aumi0 <128075224+Aumi0@users.noreply.github.com>
2023-03-17 09:57:36 +0000

    done

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
-{
-    
-    printf("bruih");
+#include <xlsxwriter.h>
+
+int main() {
+    // Open a new Excel workbook
+    lxw_workbook *workbook = workbook_new("output.xlsx");
+
+    // Add a new worksheet
+    lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
+
+    // Prompt for user input
+    char input[100];
+    printf("Enter data to put in Excel file: ");
+    scanf("%s", input);
+
+    // Write the input to cell A1 on the worksheet
+    worksheet_write_string(worksheet, 0, 0, input, NULL);
+
+    // Save the workbook and free memory
+    workbook_close(workbook);
     return 0;
 }
