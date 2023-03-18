065b962854bbec34b16c9d8a35e58ae0b0752cf0
Aumi0 <128075224+Aumi0@users.noreply.github.com>
2023-03-18 13:34:01 +0000

    done

--- the_gun.py
+++ the_gun.py
@@ -2,18 +2,22 @@ import os
 import shutil
 import random
 
-# take input from user for the folder they want to delete
-folder_path = input("Enter the path of the folder you want to delete: ")
+while True:
+    # take input from user for the folder they want to delete
+    folder_path = input("Enter the path of the folder you want to delete, or press q to quit: ")
+    
+    if folder_path == "q":
+        break
+    
+    # here is the bullet
+    random_number = random.randint(0, 5)
 
-# here is the bullets 
-random_number = random.randint(0, 5)
-
-# if the bullet works
-if random_number == 1:
-    try:
-        shutil.rmtree(folder_path)
-        print("Folder deleted successfully.")
-    except OSError as e:
-        print(f"Error deleting folder: {e}")
-else:
-    print("Deletion unsuccessful - random number was not 1.")
+    # if the bullet works
+    if random_number == 1:
+        try:
+            shutil.rmtree(folder_path)
+            print("Folder deleted successfully.")
+        except OSError as e:
+            print(f"Error deleting folder: {e}")
+    else:
+        print("Deletion unsuccessful - random number was not 1.")
