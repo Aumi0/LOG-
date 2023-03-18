58238dbea13f8a2e29314ad8ef14aaa614ff434b
Aumi0 <128075224+Aumi0@users.noreply.github.com>
2023-03-18 13:38:37 +0000

    done

--- C_gun.c
+++ C_gun.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-const char* folder_path = "/path/to/folder";
+#include <time.h>
+#include <string.h>
 
 int main() {
-    int random_number;
+    char folder_path[100];
 
-)
-    srand(time(NULL));
-    random_number = rand() % 6;
+    while (1) {
+        printf("Enter the path of the folder to delete (or type 'q' to quit): ");
+        scanf("%s", folder_path);
 
-
-    if (random_number == 1) {
-        int status = system("rm -rf /path/to/folder");
-        if (status == 0) {
-            printf("Folder deleted successfully.\n");
-        } else {
-            printf("Error deleting folder.\n");
+        if (strcmp(folder_path, "q") == 0) {
+            break;
         }
-    } else {
-        printf("Deletion unsuccessful - random number was not 1.\n");
+
+        int random_number;
+
+        srand(time(NULL));
+        random_number = rand() % 6;
+
+        if (random_number == 1) {
+            char command[100];
+            sprintf(command, "rm -rf %s", folder_path);
+            int status = system(command);
+            if (status == 0) {
+                printf("Folder deleted successfully.\n");
+            } else {
+                printf("Error deleting folder.\n");
+            }
+        } else {
+            printf("Deletion unsuccessful - random number was not 1.\n");
+        }
     }
 
     return 0;
